8d76db46cfa965324024890af11001d21273a58f
m-etemadi <m.etemadi92@gmail.com>
2023-11-10 13:58:45 +1100

    Lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShoppingProvider } from './contexts/ShoppingContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './route/ProtectedRoute';
 
-import Navbar from './components/common/Navbar/Navbar';
-import Products from './pages/Products/Product';
-import Contact from './pages/Contact/Contact';
-import Login from './pages/Login/Login';
-import Cart from './pages/Cart/Cart';
-import Order from './pages/Order/Order';
+import Navbar from './components/common/Navbar';
+import SpinnerFullPage from './components/Common/SpinnerFullPage';
+
+const Products = lazy(() => import('./pages/Products/Product'));
+const Contact = lazy(() => import('./pages/Contact/Contact'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Cart = lazy(() => import('./pages/Cart/Cart'));
+const Order = lazy(() => import('./pages/Order/Order'));
+
+// import Products from './pages/Products/Product';
+// import Contact from './pages/Contact/Contact';
+// import Login from './pages/Login/Login';
+// import Cart from './pages/Cart/Cart';
+// import Order from './pages/Order/Order';
+
 import Checkout from './pages/Order/Checkout';
 import OrderSuccess from './pages/Order/OrderSuccess';
-import ProtectedRoute from './route/ProtectedRoute';
 
 function App() {
   return (
@@ -19,26 +29,31 @@ function App() {
         <BrowserRouter>
           <Navbar />
 
-          <Routes>
-            <Route path="/" element={<Products />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="login" element={<Login />} />
-            <Route path="cart" element={<Cart />} />
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/" element={<Products />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="login" element={<Login />} />
+              <Route path="cart" element={<Cart />} />
 
-            <Route
-              path="order"
-              element={
-                <ProtectedRoute>
-                  <Order />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Checkout />} />
-              <Route path="checkout" element={<Checkout />} />
-              <Route path="order-success" element={<OrderSuccess />} />
-              <Route path="order-success/:orderId" element={<OrderSuccess />} />
-            </Route>
-          </Routes>
+              <Route
+                path="order"
+                element={
+                  <ProtectedRoute>
+                    <Order />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Checkout />} />
+                <Route path="checkout" element={<Checkout />} />
+                <Route path="order-success" element={<OrderSuccess />} />
+                <Route
+                  path="order-success/:orderId"
+                  element={<OrderSuccess />}
+                />
+              </Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </ShoppingProvider>
     </AuthProvider>
---
+++ src/components/Common/SpinnerFullPage.jsx
@@ -0,0 +1,9 @@
+function SpinnerFullPage() {
+  return (
+    <div className="spinner-fullpage">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default SpinnerFullPage;
